b6354a285d957fc90eb99514452c44a742341667
bluepill5 <alexandro.mayoral@gmail.com>
2023-10-16 22:00:43 -0600

    Api python

--- scripts/api_download.py
+++ scripts/api_download.py
@@ -1,39 +1,17 @@
+# pip install yfinance
+import yfinance as yf
 
-if __name__ == '__main__':
-    lista = [1, 2, 3, 4]
-    iterador = iter(lista)
+import requests
+from datetime import datetime
+import pandas as pd
 
-    print(type(iterador))
+# Define a start date and End Date
+start = '2021-01-01'
+#setting today date as End Date
+end = datetime.today().strftime('%Y-%m-%d')
 
-    try:
-        while True:
-            print(iterador.__next__())
-    except:
-        print('Ya no hay elementos')
+# get historical market data
+Apple_stock_price = yf.download(tickers="AAPL", start= start , end = end)
 
-class Invertir:
-    def __init__(self, cadena):
-        self.cadena = cadena
-        self.indice = len(cadena)
-    
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        if self.indice == 0:
-            self.indice = len(self.cadena)
-            raise StopIteration
-        self.indice = self.indice - 1
-        return self.cadena[self.indice]
-
-# from collections import Iterable
-cadena_inveertida = Invertir('Hola, mundo')
-for caracter in cadena_inveertida:
-    print(caracter)
-
-print(list(cadena_inveertida))
-
-
-
-
+print(Apple_stock_price)
 
